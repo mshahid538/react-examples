e13b35933adaf0646cb32914edcbcbdeb6ae8437
mshahid538 <m.shahid538@yahoo.com>
2022-03-02 19:26:08 +0500

    Basic Designing-Home-Dashboard-Signin-Signup with basic routing.

---
+++ user-managment-example/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signin from "./components/signin/Signin";
+import Signup from "./components/signup/Signup";
+import Home from "./components/home/Home";
+import Dashboard from "./components/dashboard/Dashboard";
+import Tabs from './components/common/tabs/Tabs';
+
+function App() {
+  return (
+    <>
+      <Router>
+      <Tabs/>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ user-managment-example/src/components/routes/ProtectedRoutes.js
@@ -0,0 +1,13 @@
+// import {Navigate, Route} from 'react-router-dom';
+
+// const ProtectedRoutes= props =>{
+//     const isUserLogedIn=false;
+
+//     // if(!isUserLogedIn){
+//     //     return <Navigate to='/' />
+//     // }
+
+//     return <Route {...props} />;
+// }
+
+// export default ProtectedRoutes;
