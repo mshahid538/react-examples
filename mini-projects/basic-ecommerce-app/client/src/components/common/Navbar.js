53be38658114065a60169d5ee7a6a5d670267cf5
Qasim Shah <shahlogic4u@gmail.com>
2022-03-22 17:28:22 +0500

    commented code removed

--- mini-projects/basic-ecommerce-app/client/src/components/common/Navbar.js
+++ mini-projects/basic-ecommerce-app/client/src/components/common/Navbar.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { logout } from "../../features/AuthSlice";
 import { getTotals } from "../../features/CartSlice";
 
 const Navbar = () => {
-  const { user } = useSelector((state) => state.auth);
   const { cartTotalQuantity } = useSelector((state) => state.cart);
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   React.useEffect(() => {
     dispatch(getTotals());
   }, [cart, dispatch]);
-
-  const handleLogout = () => {
-    dispatch(logout());
-    navigate("/login");
-  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
@@ -36,40 +28,31 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ml-auto">
-            {!user ? (
-              <div className="navbar-nav ml-auto">
-                <Link
-                  to="/cart"
-                  type="button"
-                  className="nav-item nav-link position-relative border-0"
-                >
-                  <i className="bi bi-bag"></i>Cart
-                  <span className="position-absolute top-1 translate-middle badge rounded-pill bg-danger">
-                    {cartTotalQuantity}
-                  </span>
-                </Link>
-                <Link to="/items" className="nav-item nav-link border-0">
-                  <i className="bi bi-diagram-2"></i>Items
-                </Link>
+            <div className="navbar-nav ml-auto">
+              <Link
+                to="/cart"
+                type="button"
+                className="nav-item nav-link position-relative border-0"
+              >
+                <i className="bi bi-bag"></i>Cart
+                <span className="position-absolute top-1 translate-middle badge rounded-pill bg-danger">
+                  {cartTotalQuantity}
+                </span>
+              </Link>
+              <Link to="/items" className="nav-item nav-link border-0">
+                <i className="bi bi-diagram-2"></i>Items
+              </Link>
 
-                <Link to="/dashboard" className="nav-item nav-link border-0">
-                  <i className="bi bi-speedometer"></i> Dashboard
-                </Link>
-                <Link
-                  to="/login"
-                  className="nav-item nav-link border-0"
-                  onClick={handleLogout}
-                >
-                  <i className="bi bi-box-arrow-right"></i> Logout
-                </Link>
-              </div>
-            ) : (
-              <>
-                <Link to="/login" className="nav-item nav-link">
-                  <i className="bi bi-person"></i> Login
-                </Link>
-              </>
-            )}
+              <Link to="/dashboard" className="nav-item nav-link border-0">
+                <i className="bi bi-speedometer"></i> Dashboard
+              </Link>
+              <Link to="/login" className="nav-item nav-link border-0">
+                <i className="bi bi-box-arrow-right"></i> Logout
+              </Link>
+              <Link to="/login" className="nav-item nav-link">
+                <i className="bi bi-person"></i> Login
+              </Link>
+            </div>
           </div>
         </div>
       </div>
