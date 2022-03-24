ba4c987618ba6c447289f34c2bf0ffbb4beb66ab
Qasim Shah <shahlogic4u@gmail.com>
2022-03-24 17:46:06 +0500

    prettier configured

--- mini-projects/basic-ecommerce-app/client/src/features/AuthSlice.js
+++ mini-projects/basic-ecommerce-app/client/src/features/AuthSlice.js
@@ -1,10 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "../services/AuthService";
 
-const user = JSON.parse(localStorage.getItem("user"));
-
 const initialState = {
-  user: user ? user : null,
+  user: null,
 };
 
 // Login user
