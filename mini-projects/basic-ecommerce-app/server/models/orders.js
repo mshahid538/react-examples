bf0153b643c3d0e8d039580ad6030f39614ac47d
Qasim Shah <shahlogic4u@gmail.com>
2022-03-28 13:12:12 +0500

    Order model + route created

--- mini-projects/basic-ecommerce-app/server/models/orders.js
+++ mini-projects/basic-ecommerce-app/server/models/orders.js
@@ -1,24 +1,30 @@
 const mongoose = require("mongoose");
 
-const ordersSchema = new mongoose.Schema({
-  userId: { type: String },
-  items: [
-    {
-      itemId: {
-        type: String,
-      },
-      quantity: {
-        type: Number,
-        default: 1,
-      },
-      balance: {
-        type: Number,
-      },
+const ordersSchema = new mongoose.Schema(
+  {
+    Buyers: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Buyers",
     },
-  ],
-  dateAdded: {
-    type: String,
-  }, 
-});
+    orderitems: [
+      {
+        Items: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Items",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        name: { type: String },
+      },
+    ],
+
+    balance: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Orders", ordersSchema);
