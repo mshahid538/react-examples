2d930377a93987c67e9b14bc68bf2d659c0697b6
Qasim Shah <shahlogic4u@gmail.com>
2022-03-30 14:53:33 +0500

    authservice update

--- mini-projects/basic-ecommerce-app/client/src/services/AuthService.js
+++ mini-projects/basic-ecommerce-app/client/src/services/AuthService.js
@@ -4,6 +4,10 @@ import { API_LOGIN_URL } from "../constants";
 const login = async (userData) => {
   const response = await axios.post(API_LOGIN_URL, userData);
 
+  if (response?.status !== 200) {
+    return response.error(`Error: ${response.statusText}`);
+  }
+
   if (response.data) {
     return response.data;
   }
